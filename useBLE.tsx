947b45a189bee013291fc40be7e60be2b37eff96
mertsenn <mert.sen300fb@gmail.com>
2022-12-03 22:44:31 +0300

    first commit

---
+++ useBLE.tsx
@@ -0,0 +1,66 @@
+import {useState} from 'react';
+import {PermissionsAndroid, Platform} from 'react-native';
+import {BleManager, Device} from 'react-native-ble-plx';
+
+const bleManager = new BleManager();
+
+type VoidCallback = (result: boolean) => void;
+
+interface BluetoothLowEnergyApi {
+  requestPermissions(cb: VoidCallback): Promise<void>;
+  scanForPeripherals(): void;
+  allDevices: Device[];
+  connectToDevice: (deviceId: string) => void;
+}
+
+function useBLE(): BluetoothLowEnergyApi {
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+
+  const requestPermissions = async (cb: VoidCallback) => {
+    if (Platform.OS === 'android') {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Permission',
+          message: 'Bluetooth Low Energy requires Location',
+          buttonNeutral: 'Ask Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      cb(granted === PermissionsAndroid.RESULTS.GRANTED);
+    } else {
+      cb(true);
+    }
+  };
+
+  const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
+    devices.findIndex(device => nextDevice.id === device.id) > -1;
+
+  const scanForPeripherals = () =>
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error);
+      }
+      if (device && device.name?.includes('CorSense')) {
+        setAllDevices((prevState: Device[]) => {
+          if (!isDuplicteDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+      }
+    });
+
+  const connectToDevice = (deviceId: string) =>
+    bleManager.connectToDevice(deviceId);
+
+  return {
+    scanForPeripherals,
+    requestPermissions,
+    connectToDevice,
+    allDevices,
+  };
+}
+
+export default useBLE;
